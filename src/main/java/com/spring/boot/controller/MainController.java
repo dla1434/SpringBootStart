e3bb521420b3b1eb5c1dbe0b9aeadfdbfe501add
ljh <dla1434@naver.com>
2018-06-17 02:59:26 +0900

    JPA 기본 적용

--- src/main/java/com/spring/boot/controller/MainController.java
+++ src/main/java/com/spring/boot/controller/MainController.java
@@ -1,9 +1,19 @@
 package com.spring.boot.controller;
 
+import java.util.Date;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.spring.boot.model.Task;
+import com.spring.boot.service.TaskService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,9 +21,42 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class MainController {
 	
+	@Autowired
+	private TaskService taskService;
+	
 	@GetMapping("/")
 	public String home(HttpServletRequest request) {
 		log.info("Web Server Root Start");
+		
+		return "index";
+	}
+	
+	@GetMapping("/all-tasks")
+	public String allTasks(HttpServletRequest request) {
+		log.info("allTasks");
+		
+		List<Task> findAll = taskService.findAll();
+		log.info("tasks allTasks : {}", findAll);
+		
+		return "index";
+	}
+	
+	@GetMapping("/save-task")
+	public String saveTask(HttpServletRequest request, @ModelAttribute Task task, BindingResult bindResult) {
+		log.info("saveTask");
+		
+		task.setDateCreated(new Date());
+		task.setName("jpaTest");
+		taskService.save(task);
+		
+		return "index";
+	}
+	
+	@GetMapping("/delete-task")
+	public String deleteTask(HttpServletRequest request, @RequestParam int id) {
+		log.info("deleteTask");
+		
+		taskService.delete(id);
 		
 		return "index";
 	}
