96161b3baf87c10e0eec0cf8086545fd6471634a
ljh <dla1434@naver.com>
2018-06-17 04:01:36 +0900

    JPA 처리 Controller 수정 JPA 처리 페이지 추가 완료

--- src/main/java/com/spring/boot/controller/MainController.java
+++ src/main/java/com/spring/boot/controller/MainController.java
@@ -7,9 +7,11 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.spring.boot.model.Task;
@@ -25,39 +27,68 @@ public class MainController {
 	private TaskService taskService;
 	
 	@GetMapping("/")
-	public String home(HttpServletRequest request) {
+	public String home(HttpServletRequest request, ModelMap modelMap) {
 		log.info("Web Server Root Start");
-		request.setAttribute("page", "fragments/main");
+		
+		modelMap.addAttribute("page", "fragments/main");
 		
 		return "index";
 	}
 	
 	@GetMapping("/all-tasks")
-	public String allTasks(HttpServletRequest request) {
+	public String allTasks(HttpServletRequest request, ModelMap modelMap) {
 		log.info("allTasks");
 		
 		List<Task> findAll = taskService.findAll();
 		log.info("tasks allTasks : {}", findAll);
 		
+		modelMap.addAttribute("page", "fragments/task/taskList");
+		modelMap.addAttribute("tasks", findAll);
+		
 		return "index";
 	}
 	
-	@GetMapping("/save-task")
-	public String saveTask(HttpServletRequest request, @ModelAttribute Task task, BindingResult bindResult) {
+	@GetMapping("/new-task")
+	public String newTask(HttpServletRequest request, ModelMap modelMap) {
+		log.info("newTask");
+		
+		modelMap.addAttribute("page", "fragments/task/taskView");
+		
+		return "index";
+	}
+	
+	
+	@PostMapping("/save-task")
+	public String saveTask(HttpServletRequest request, @ModelAttribute Task task, BindingResult bindResult, ModelMap modelMap) {
 		log.info("saveTask");
 		
 		task.setDateCreated(new Date());
-		task.setName("jpaTest");
 		taskService.save(task);
+		
+		modelMap.addAttribute("tasks", taskService.findAll());
+		modelMap.addAttribute("page", "fragments/task/taskList");
+		
+		return "index";
+	}
+	
+	@GetMapping("/update-task")
+	public String updateTask(HttpServletRequest request, ModelMap modelMap, @RequestParam int id) {
+		log.info("newTask");
+		
+		modelMap.addAttribute("task", taskService.findTask(id));
+		modelMap.addAttribute("page", "fragments/task/taskView");
 		
 		return "index";
 	}
 	
 	@GetMapping("/delete-task")
-	public String deleteTask(HttpServletRequest request, @RequestParam int id) {
+	public String deleteTask(HttpServletRequest request, ModelMap modelMap, @RequestParam int id) {
 		log.info("deleteTask");
 		
 		taskService.delete(id);
+		
+		modelMap.addAttribute("tasks", taskService.findAll());
+		modelMap.addAttribute("page", "fragments/task/taskList");
 		
 		return "index";
 	}
