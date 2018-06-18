50bd41a9b2d30b9b23dc5c6d6fdfc60d7748bf03
ljh <dla1434@naver.com>
2018-06-18 21:28:29 +0900

    View Controller, CRUD Controller 분리

--- src/main/java/com/spring/boot/controller/MainController.java
+++ src/main/java/com/spring/boot/controller/MainController.java
@@ -35,38 +35,11 @@ public class MainController {
 		return "index";
 	}
 	
-	@GetMapping("/all-tasks")
-	public String allTasks(HttpServletRequest request, ModelMap modelMap) {
-		log.info("allTasks");
-		
-		List<Task> findAll = taskService.findAll();
-		log.info("tasks allTasks : {}", findAll);
-		
-		modelMap.addAttribute("page", "fragments/task/taskList");
-		modelMap.addAttribute("tasks", findAll);
-		
-		return "index";
-	}
-	
 	@GetMapping("/new-task")
 	public String newTask(HttpServletRequest request, ModelMap modelMap) {
 		log.info("newTask");
 		
 		modelMap.addAttribute("page", "fragments/task/taskView");
-		
-		return "index";
-	}
-	
-	
-	@PostMapping("/save-task")
-	public String saveTask(HttpServletRequest request, @ModelAttribute Task task, BindingResult bindResult, ModelMap modelMap) {
-		log.info("saveTask");
-		
-		task.setDateCreated(new Date());
-		taskService.save(task);
-		
-		modelMap.addAttribute("tasks", taskService.findAll());
-		modelMap.addAttribute("page", "fragments/task/taskList");
 		
 		return "index";
 	}
@@ -77,18 +50,6 @@ public class MainController {
 		
 		modelMap.addAttribute("task", taskService.findTask(id));
 		modelMap.addAttribute("page", "fragments/task/taskView");
-		
-		return "index";
-	}
-	
-	@GetMapping("/delete-task")
-	public String deleteTask(HttpServletRequest request, ModelMap modelMap, @RequestParam int id) {
-		log.info("deleteTask");
-		
-		taskService.delete(id);
-		
-		modelMap.addAttribute("tasks", taskService.findAll());
-		modelMap.addAttribute("page", "fragments/task/taskList");
 		
 		return "index";
 	}
