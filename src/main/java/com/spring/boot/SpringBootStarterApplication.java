0e36ba36dc38518ee9b0392bc82c96d7d88a5e15
dla1434 <dla1434@naver.com>
2018-06-18 15:18:28 +0900

    First Commit

--- src/main/java/com/spring/boot/SpringBootStarterApplication.java
+++ src/main/java/com/spring/boot/SpringBootStarterApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootStarterApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootStarterApplication.class, args);
 	}
