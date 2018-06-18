1c8afeedffd60abb9c880cc73f27428f4d18910b
dla1434 <dla1434@naver.com>
2018-06-18 15:38:04 +0900

    Repository 클래스 구현 - ElasticsearchRepository 상속 - DAO랑 동일하다고 생각하면 된다.

---
+++ src/main/java/com/spring/boot/repository/BookRepository.java
@@ -0,0 +1,15 @@
+package com.spring.boot.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import com.spring.boot.model.Book;
+
+public interface BookRepository extends ElasticsearchRepository<Book, String>{
+	Page<Book> findByAuthor(String author, Pageable pageable);
+	
+	List<Book> findByTitle(String title);
+}
